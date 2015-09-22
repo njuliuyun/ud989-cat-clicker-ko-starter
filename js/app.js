42dc25aefa91d76ed9f429d305bdc5e3cc91e0a8
njuliuyun <njuliuyun@hotmail.com>
2015-09-21 22:57:31 -0400

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable("Tabby");
+    this.imgSrc = ko.observable("img/2.jpg");
+    
+    this.increamentCount = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+    
+    this.level = ko.computed(function() {
+        if (this.clickCount() < 10) return "Newborn";
+        else if (this.clickCount() < 20) return "Infant";
+        else if (this.clickCount() < 30) return "Teen";
+        else return "Adult";
+    }, this);
+}
+ko.applyBindings(new ViewModel());
