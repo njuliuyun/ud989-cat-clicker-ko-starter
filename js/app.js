7c328b17666c65215c696cdd9d2f6122c270a304
njuliuyun <njuliuyun@hotmail.com>
2015-09-22 11:56:32 -0400

    add more cats

--- js/app.js
+++ js/app.js
@@ -1,7 +1,41 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable("Tabby");
-    this.imgSrc = ko.observable("img/2.jpg");
+var initialCats = [
+    {
+        name: "Tabby",
+        clickCount: 0,
+        imgSrc: "img/1.jpg",
+        nickNames: ["TT", "Tad", "Mr. T"]
+    },
+    {
+        name: "Tiger",
+        clickCount: 0,
+        imgSrc: "img/2.jpg",
+        nickNames: ["Tiger"]
+    },
+    {
+        name: "Scaridy",
+        clickCount: 0,
+        imgSrc: "img/4.jpg",
+        nickNames: ["Casper"]
+    },
+    {
+        name: "Shadow",
+        clickCount: 0,
+        imgSrc: "img/3.jpg",
+        nickNames: ["Shooby"]
+    },
+    {
+        name: "Sleepy",
+        clickCount: 0,
+        imgSrc: "img/5.jpg",
+        nickNames: ["Zzzz"]
+    },
+];
+
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.nickNames = ko.observableArray(data.nickNames);
     
     this.level = ko.computed(function() {
         if (this.clickCount() < 10) return "Newborn";
@@ -10,13 +44,24 @@ var Cat = function() {
         else return "Adult";
     }, this);
     
-    this.nickNames = ko.observableArray(["TT", "Tad", "Mr. T"]);
 }
 
 var ViewModel = function() {
-    this.currentCat = ko.observable(new Cat());
+    var self = this;
+    this.catList = ko.observableArray([]);
+    
+    initialCats.forEach(function(catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+    
+    this.currentCat = ko.observable(this.catList()[0]);
+    
     this.increamentCount = function() {
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+    
+    this.setCurrCat = function(cat) { 
+        self.currentCat(cat);
     };
 }
 ko.applyBindings(new ViewModel());
